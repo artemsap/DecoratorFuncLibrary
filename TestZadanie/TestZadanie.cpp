853d3f128355e4362a174ca5507e54132c467237
ArtemSap <artik21rus21@gmail.com>
2023-02-21 20:21:00 +0300

    first commit

---
+++ TestZadanie/TestZadanie.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <functional>
+#include <type_traits>
+
+#include "func_decorators.h"
+
+class SimpleFuncator
+{
+public:
+	void operator()()
+	{
+		std::cout << "Hello world" << std::endl;
+	}
+};
+
+void foo(std::string str)
+{
+	std::cout << "Hello " << str << std::endl;
+}
+
+int main()
+{
+	auto lambdafoo = []() {
+		std::cout << "Hello world" << std::endl;
+	};
+
+	auto lambdafoo_1 = [](std::string name) {
+		std::cout << "Hello " << name << std::endl;
+	};
+
+	auto lambdafoo_2 = [](std::string name) {
+		return name;
+	};
+
+	auto lambdafoo_3 = [](std::size_t n, std::string const& name)
+	{
+		for (std::size_t i = 0; i != n; ++i) {
+			std::cout << "Hello " << name << std::endl;
+		};
+	};
+
+	SimpleFuncator a;
+
+	auto dec11 = decorate_tracelog(
+		[](std::size_t n, std::string const& name)
+		{
+			for (std::size_t i = 0; i != n; ++i) {
+				std::cout << "Hello " << name << std::endl;
+			};
+		}
+	);
+	dec11(3, "name");
+	
+	std::cout << std::endl;
+
+	auto dec12 = decorate_printres(lambdafoo_2);
+	auto name12 = dec12("name");
+
+	std::cout << std::endl;
+
+	auto dec13 = decorate_tracelog(decorate_printres(lambdafoo_2));
+	auto name13 = dec13("name");
+
+	std::cout << std::endl;
+
+	auto dec14 = decorate_tracelog(decorate_printres(a));
+	dec14();
+
+	std::cout << std::endl;
+
+	auto dec15 = decorate_exception(
+		[]()
+		{
+			int num;
+			std::cin >> num;
+			if (num < 0)
+				throw - 1;
+			else
+				return num;
+		});
+	dec15();
+}
---
+++ TestZadanie/func_decorators.h
@@ -0,0 +1,156 @@
+#pragma once
+#include <iostream>
+
+template <class func>
+class BaseDecoratorFunc
+{
+private:
+	func functor;
+
+	template <typename ...Args>
+	using ret_type = std::result_of_t<decltype(functor)(Args...)>;
+public:
+	BaseDecoratorFunc(func _functor) : functor(_functor) {}
+
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> ret_type<Args...>
+	{
+		return functor(method_args...);
+	}
+};
+
+template <class func>
+class TraceLogDecoratorFunc
+{
+private:
+	BaseDecoratorFunc<func> baseDec;
+
+	template <typename ...Args>
+	using ret_type = std::result_of_t<decltype(baseDec)(Args...)>;
+	
+	void PrintArgs()
+	{
+		std::cout << "No input arguments" << std::endl;
+	}
+
+	template<typename Arg>
+	void PrintArgs(Arg first)
+	{
+		std::cout << '[' << first << ']' << std::endl;
+	}
+
+	template<typename Arg, typename... Args>
+	void PrintArgs(Arg first, Args... others)
+	{
+		std::cout << '[' << first << "] ";
+		PrintArgs(others...);
+	}
+
+	template <typename ...Args>
+	void DecoratorProcess(std::string start_message, Args... method_args)
+	{
+		std::cout << start_message;
+		PrintArgs(method_args...);
+	}
+
+public:
+	TraceLogDecoratorFunc(func _functor) : baseDec(_functor){}
+	
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> typename std::enable_if<std::is_void_v<ret_type<Args...>>>::type
+	{
+		DecoratorProcess("decorate_tracelog: Start calling with args: ", method_args...);
+		baseDec(method_args...);
+		DecoratorProcess("decorate_tracelog: End calling with args: ", method_args...);
+	}
+
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> typename std::enable_if<!std::is_void_v<ret_type<Args...>>, ret_type<Args...>>::type
+	{
+		DecoratorProcess("decorate_tracelog: Start calling with args: ", method_args...);
+		auto ret = baseDec(method_args...);
+		DecoratorProcess("decorate_tracelog: End calling with args: ", method_args...);
+		return ret;
+	}
+};
+
+template<class T>
+TraceLogDecoratorFunc<T> decorate_tracelog(T func)
+{
+	return TraceLogDecoratorFunc<T>(func);
+}
+
+template <class func>
+class PrintResultDecoratorFunc
+{
+private:
+	BaseDecoratorFunc<func> baseDec;
+
+	template <typename ...Args>
+	using ret_type = std::result_of_t<decltype(baseDec)(Args...)>;
+
+public:
+	PrintResultDecoratorFunc(func _functor) : baseDec(_functor) {}
+
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> typename std::enable_if<std::is_void_v<ret_type<Args...>>>::type
+	{
+		baseDec(method_args...);
+		std::cout << "decorate_printres: No result. Function has void return type" << std::endl;
+	}
+
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> typename std::enable_if<!std::is_void_v<ret_type<Args...>>, ret_type<Args...>>::type
+	{
+		auto ret = baseDec(method_args...);
+		std::cout << "decorate_printres: Result of function:" << " [" << ret << ']' << std::endl;
+		return ret;
+	}
+};
+
+template<class T>
+PrintResultDecoratorFunc<T> decorate_printres(T func)
+{
+	return PrintResultDecoratorFunc<T>(func);
+}
+
+template <class func>
+class CatchExceptionDecoratorFunc
+{
+private:
+	BaseDecoratorFunc<func> baseDec;
+
+	template <typename ...Args>
+	using ret_type = std::result_of_t<decltype(baseDec)(Args...)>;
+
+public:
+	CatchExceptionDecoratorFunc(func _functor) : baseDec(_functor) {}
+
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> typename std::enable_if<std::is_void_v<ret_type<Args...>>>::type
+	{
+		baseDec(method_args...);
+	}
+
+	template <typename ...Args>
+	auto operator() (Args... method_args) -> typename std::enable_if<!std::is_void_v<ret_type<Args...>>, ret_type<Args...>>::type
+	{
+		try 
+		{
+			auto ret = baseDec(method_args...);
+			std::clog << "NO ERROR IN FUNCTION" << std::endl;
+			return ret;
+		}
+		catch (...)
+		{
+			std::clog << "ERROR IN FUNCTION" << std::endl;
+			return ret_type<Args...>();
+		}
+	}
+};
+
+template<class T>
+CatchExceptionDecoratorFunc<T> decorate_exception(T func)
+{
+	return CatchExceptionDecoratorFunc<T>(func);
+}
